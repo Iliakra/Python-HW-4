7464b9903264903b84639674a1659f068c61fc26
Iliakra <kravus27@gmail.com>
2021-10-07 16:45:04 +0300

    task_1 added

---
+++ task_1.py
@@ -0,0 +1,18 @@
+""" Красильников Илья
+1. Реализовать скрипт, в котором должна быть предусмотрена функция расчета заработной платы сотрудника. В расчете
+необходимо использовать формулу: (выработка в часах * ставка в час) + премия. Для выполнения расчета для конкретных
+значений необходимо запускать скрипт с параметрами.
+"""
+
+from sys import argv
+
+
+script_name, production_hours, hour_rate, prize = argv
+
+
+def salary_calculate(script_name, production_hours, hour_rate, prize):
+    result = (float(production_hours) * float(hour_rate)) + float(prize)
+    print(f"Заработная плата, рассчитанная в {script_name} равна {result}")
+
+
+salary_calculate(script_name, production_hours, hour_rate, prize)
